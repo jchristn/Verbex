634618e10721085d0a3cc93931d21a6698b26321
Joel Christner <joel.christner@gmail.com>
2026-07-20 09:35:09 -0700

    Remove Docker Cloud builder from build scripts

--- build-server.sh
+++ build-server.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <tag>"
     exit 1
 fi
-docker buildx build --builder cloud-jchristn77-jchristn77 --platform linux/amd64,linux/arm64/v8 --provenance=false --sbom=false -t jchristn77/verbex-server:$1 -t jchristn77/verbex-server:latest --push -f src/Verbex.Server/Dockerfile src
+docker buildx build --platform linux/amd64,linux/arm64/v8 --provenance=false --sbom=false -t jchristn77/verbex-server:$1 -t jchristn77/verbex-server:latest --push -f src/Verbex.Server/Dockerfile src
